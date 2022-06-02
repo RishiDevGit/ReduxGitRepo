c6ab857f83ae46829992f15a12f7a4107dfa86b9
RishiDevGit <rishirajsaxena@makewebbetter.com>
2022-06-02 09:33:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { useEffect } from "react";
-import { useSelector,useDispatch } from "react-redux";
-import axios from 'axios';
-import { fetchPostsData, fetchPostsDataStarted } from "./Posts/actions";
 import PostsAndUsersPage from "./PostsAndUsersPage";
  
 const App=()=>{
